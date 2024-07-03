3c587803c20efcadb7dcce0d9fb1baab2a3c8f93
Ng Ngoc Thach <thach.nguyen@beready.academy>
2024-07-03 11:53:35 +0700

    Yii 2 Guide

--- config/db.php
+++ config/db.php
@@ -1,11 +1,17 @@
 <?php
 
+use Symfony\Component\Dotenv\Dotenv;
+
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__ . '/../.env'); 
+
+
 return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
+   'class' => 'yii\db\Connection',
+   'dsn' => 'mysql:host=' . $_ENV['MYSQL_HOST'] . ';dbname=' . $_ENV['MYSQL_NAME'],
+   'username' => $_ENV['MYSQL_USER'],
+   'password' => $_ENV['MYSQL_PASS'],
+   'charset' => 'utf8',
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
